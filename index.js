54f3de449c27d0b840d43ee4988f082595ce8e78
Titus Wormer <tituswormer@gmail.com>
2016-11-24 18:34:14 +0100

    Rewrite

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 'use strict';
 
-/*
- * Dependencies.
- */
-
-var stemmer = require('wooorm/stemmer@0.1.4');
-
-/*
- * DOM nodes.
- */
+var stemmer = require('stemmer');
 
 var $input = document.getElementsByTagName('input')[0];
 var $output = document.getElementsByTagName('output')[0];
 
-/*
- * Handlers.
- */
-
-function oninputchange() {
-    $output.textContent = stemmer($input.value);
-}
-
-/*
- * Listen.
- */
-
 $input.addEventListener('input', oninputchange);
 
-/*
- * Initial answer.
- */
-
 oninputchange();
+
+function oninputchange() {
+  $output.textContent = stemmer($input.value);
+}
